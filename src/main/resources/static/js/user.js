4fbfc22d04fe69b8c1096db1d5771c028ab5eda0
JAESKAAA <jaeskaaa@gmail.com>
2021-11-08 20:52:10 +0900

    회원가입까지 완료

---
+++ src/main/resources/static/js/user.js
@@ -0,0 +1,41 @@
+let index = {
+	init:function(){
+		$("#btn-save").on("click",function(){
+		index.save();
+		});
+	},
+
+	save:function(){
+		let data = {
+			username : $("#username").val(),
+			password : $("#password").val(),
+			email : $("#email").val()
+		}
+		console.log(data);
+
+		// ajax호출시 default가 비동기 호출
+		// ajax 통신을 이용해서 3rodml 데이터를 json으로 변경하여 insert 요청
+		// ajax가 통신을 성공하고 서버가 json을 리턴해주면 자동으로 자바 오브젝트로 변환해줌
+		$.ajax({
+			//회원가입 수행 요청
+			type:"POST",
+			url:"/blog/api/user",
+			data: JSON.stringify(data), //http body 데이터
+			contentType: "application/json; charset=utf-8", //body데이터가 어떤 타입인지 (MIME)
+			dataType : "json"
+			//요청을 서버로 해서 응답이 왔을 때 기본적으로 모든것이 문자열인데, 형태가 json형태면 자바스크립트 객체로 변환해줌
+		}).done(function(resp){
+			//요청 성공하면 해당 function 실행
+			alert("회원가입이 완료되었습니다.");
+			console.log(resp);
+			location.href = "/blog";
+		}).fail(function(error){
+			//요청 실패시 해당 function 실행
+			alert(JSON.stringify(error));
+		});
+		
+	}
+}	
+
+index.init();
+
---
+++ src/main/webapp/WEB-INF/views/layout/footer.jsp
@@ -0,0 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+pageEncoding="UTF-8"%>
+<br/>
+<div class="jumbotron text-center" style="margin-bottom: 0">
+  <p>Created by jaeskaaa</p>
+  <p>📞010-000-000</p>
+  <p>🏴서울특별시 강남구</p>
+</div>
+</body>
+</html>
